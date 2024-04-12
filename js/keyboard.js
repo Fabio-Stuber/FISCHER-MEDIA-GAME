05680fb5094dc5fd82cc861b37b84a9301bcb94a
Fabio Stuber <58259376+Fabio-Stuber@users.noreply.github.com>
2024-04-12 10:43:13 +0200

    Add files via upload

---
+++ js/keyboard.js
@@ -0,0 +1,25 @@
+function keyboard(keyCode) {
+	return bbzGameKeys[keyCode];
+}
+
+function keyboardOnce(keyCode) {
+	if (bbzGameKeys[keyCode]) {
+		bbzGameKeys[keyCode] = false;
+		return true;
+	} else {
+		return false;
+	}
+}
+
+let bbzGameKeys = {};
+
+function bbzGameKeydown(e) {
+	bbzGameKeys[e.keyCode] = true;
+}
+
+function bbzGameKeyup(e) {
+	bbzGameKeys[e.keyCode] = false;
+}
+
+document.addEventListener("keydown", bbzGameKeydown);
+document.addEventListener("keyup", bbzGameKeyup);
