5977e495c8cb6fedd8e66a30a1f4b5f15ce3b0cf
Fabio Stuber <58259376+Fabio-Stuber@users.noreply.github.com>
2024-04-12 10:41:23 +0200

    Delete js directory

--- js/gameover.js
+++
@@ -1,6 +0,0 @@
-let ScoreAnzeige = document.querySelector(".Score");
-let HighScoreAnzeige = document.querySelector(".HighScore");
-let score_e = localStorage.getItem("gameover_score");
-let score_High = localStorage.getItem("HighScore");
-ScoreAnzeige.textContent = "Du hast " + score_e + " Punkte Gesammelt";
-HighScoreAnzeige.textContent = "Dein HighSore ist: " + score_High + " Punkte";
